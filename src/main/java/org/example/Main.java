d34e407cd42ca937693268365b2b8ddd607f7add
vietha <hanguyenthua07@gmail.com>
2026-06-15 23:15:15 +0700

    Add initial project files: source code, pom.xml, and configuration

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+
+import GRPC.JudgeRequest;
+import GRPC.JudgeResponse;
+import GRPC.JudgeServiceGrpc;
+import GRPC.SubmitRequest;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException, NoSuchAlgorithmException, InvalidKeyException {
+
+
+
+    }
+}
